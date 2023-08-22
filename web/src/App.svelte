4e88b6a685a59e73aefb3e954215e00a34024206
Hrólfur <hrolfurgylfa@protonmail.com>
2023-08-22 23:27:07 +0000

    Started on web version in Svelte

---
+++ web/src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import Examples from "./lib/Examples.svelte";
+  import CodeEditor from "./lib/CodeEditor.svelte";
+  import ProgramOutput from "./lib/ProgramOutput.svelte";
+</script>
+
+<main>
+  <h1 class="text-3xl text-center mt-6">TestLang2</h1>
+
+  <div class="grid grid-cols-3 gap-6 m-6">
+    <div class="col-span-2">
+      <CodeEditor />
+      <ProgramOutput />
+    </div>
+    <Examples />
+  </div>
+</main>
+
+<style>
+</style>
---
+++ web/src/lib/Examples.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  const programs = [
+    ["Comments", "comments.tstl"],
+    ["Count", "count.tstl"],
+    ["FizzBuzz", "fizzbuzz.tstl"],
+    ["Loop", "loop.tstl"],
+    ["Random", "random.tstl"],
+  ];
+
+  function selectProgram(program: string) {
+    console.log("Opening:", program);
+  }
+</script>
+
+<div>
+  <h2 class="text-xl">Example programs:</h2>
+  {#each programs as program}
+    <button on:click={(_) => selectProgram(program[1])}>
+      {program[0]}
+    </button>
+    <br />
+  {/each}
+</div>
